253dd036a95968fd7120e2bd5c44f1ea46b4833a
jLuseno161 <joyluseno2@gmail.com>
2021-09-07 09:40:16 +0300

    introducing state in react

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,11 +1,42 @@
 import React from "react"
+
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
+      // <div>
+      //   <h1>Hello from Create React App</h1>
+      //   <p>I am in a React Component!</p>
+      // </div>
+
+      // React Fragment (a virtual element that doesn’t get shown in the DOM).
+      <React.Fragment>
+      <h2>My To do List</h2>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li>{todo.title}</li>
+          ))}
+        </ul>
+      </React.Fragment>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+
+//React provides for us the StrictMode to activate checks and logs a warning message at runtime.
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
